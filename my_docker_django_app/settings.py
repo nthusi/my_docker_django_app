e2b44be756d43ea7ef8cdc9dbe7971706b80c462
nthusi <muindenthusi@gmail.com>
2025-02-20 18:21:41 +0300

    allowed hosts 2

--- my_docker_django_app/settings.py
+++ my_docker_django_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1", ".code.run").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 
 # Application definition
